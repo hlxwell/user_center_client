4fc9f1cdfd80866aff5574fe3bb5529aea641d16
Michael He <hlxwell@gmail.com>
2011-11-20 22:43:25 +0800

    add current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
+
   # This will allow the user to view the index page without authentication
   # but will process CAS authentication data if the user already
   # has an SSO session open.
@@ -8,10 +8,15 @@ class ApplicationController < ActionController::Base
 
   # This requires the user to be authenticated for viewing allother pages.
   # before_filter CASClient::Frameworks::Rails::Filter
-  
+
   # @username = session[:cas_user]
   # @extra_attributes = session[:cas_extra_attributes]
   # @username = session[:cas_user]
   # CASClient::Frameworks::Rails::Filter.logout(self)
   # @login_url = CASClient::Frameworks::Rails::Filter.login_url(self)
+
+  def current_user
+    session[:cas_user]
+  end
+  helper_method :current_user
 end
