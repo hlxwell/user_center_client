6e30b7184b15abb367f423d6eb5e6a5e1b0ac26e
Michael He <hlxwell@gmail.com>
2011-11-22 22:01:16 +0800

    change site title

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,4 +19,18 @@ class ApplicationController < ActionController::Base
     session[:cas_user]
   end
   helper_method :current_user
+  
+  def site_name
+    case
+    when request.host =~ /myasics/
+      "My ASICS SSO Demo"
+    when request.host =~ /asics/
+      "ASICS SSO Demo"
+    when request.host =~ /isafeplayer/
+      "Different Domain SSO Demo"
+    else
+      "Demo"
+    end
+  end
+  helper_method :site_name
 end
