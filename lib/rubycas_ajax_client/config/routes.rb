89dcc6405ee781c8f0ee4df45e4ce052ed75cf73
Michael He <hlxwell@gmail.com>
2011-11-26 13:20:23 +0800

    add logout page and move st checker in to gem

--- lib/rubycas_ajax_client/config/routes.rb
+++ lib/rubycas_ajax_client/config/routes.rb
@@ -2,5 +2,7 @@ module RubycasAjaxClient
   Rails.application.routes.draw do
     get "/login" => "rubycas_session#login", :as => :login
     get "/logout" => "rubycas_session#logout", :as => :logout
+
+    get "/login_st" => "rubycas_session#login_st"
   end
 end
